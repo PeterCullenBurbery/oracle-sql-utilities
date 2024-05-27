1401d8d4776388dbd58700bc5547f39bc44a1e0f
PeterCullenBurbery <peter.cullen.burbery@gmail.com>
2024-05-27 16:50:24 -0400

    Update make-all-records-uppercase-in-a-column-in-a-table.sql

--- make-all-records-uppercase-in-a-column-in-a-table.sql
+++ make-all-records-uppercase-in-a-column-in-a-table.sql
@@ -1,11 +1,25 @@
-CREATE OR REPLACE PROCEDURE Uppercase_Column_Entries (
-    p_table_name IN VARCHAR2,
+CREATE OR REPLACE PROCEDURE uppercase_column_entries (
+    p_table_name  IN VARCHAR2,
     p_column_name IN VARCHAR2
 ) AS
     l_sql_statement VARCHAR2(1000);
 BEGIN
-    l_sql_statement := 'UPDATE ' || p_table_name || 
-                       ' SET ' || p_column_name || ' = UPPER(' || p_column_name || ')';
+    l_sql_statement := 'UPDATE '
+                       || p_table_name
+                       || ' SET '
+                       || p_column_name
+                       || ' = UPPER('
+                       || p_column_name
+                       || ')';
+
     EXECUTE IMMEDIATE l_sql_statement;
     COMMIT;
-END Uppercase_Column_Entries;
+END uppercase_column_entries;
+/
+
+BEGIN
+    uppercase_column_entries(
+                            'your_table_name',
+                            'your_column_name'
+    );
+END;
